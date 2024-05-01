f3d698c2a056c922e2d25e486fa07e4893aef7f2
João Victor Macedo Fernandes Ramos <73120990+JoaoFernandesz@users.noreply.github.com>
2024-05-01 14:06:31 +0000

    finaliza header e menu

--- kero-mais-acai/app/page.js
+++ kero-mais-acai/app/page.js
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <>
       <Header />
-      {/* Outros componentes da página aqui */}
+      
     </>
   );
 }
--- kero-mais-acai/components/Header.js
+++ kero-mais-acai/components/Header.js
@@ -1,4 +1,3 @@
-
 import Link from 'next/link';
 import styles from './Header.module.css';
 
@@ -6,29 +5,30 @@ export default function Header() {
     return (
         <header className={styles.header}>
             <div className={styles.logoContainer}>
-                <img src="../img/logo.png" alt="Logo" className={styles.logo} />
+                <img src="/img/logo.png" alt="Logo" className={styles.logo} />
             </div>
-
             <nav className={styles.nav}>
-                <Link href="/sobre" passHref><span className={styles.link}>Sobre</span></Link>
-                <Link href="/produtos" passHref><span className={styles.link}>Produtos</span></Link>
-                <Link href="/contato" passHref><span className={styles.link}>Contato</span></Link>
+                <Link className={styles.link} href="/Inicio" passHref><span >Inicio</span></Link>
+                <Link className={styles.link} href="/Sobre" passHref><span >Sobre</span></Link>
+                <Link className={styles.link} href="/contato" passHref><span >Contato</span></Link>
             </nav>
-            <Link href="/comprar" passHref><span className={styles.cta}>Compre Aqui</span></Link>
-
+            <Link className={styles.cta} href="/comprar" passHref><span className={styles.cta}>Peça Já</span></Link>
             <input type="checkbox" id="menu-toggle" className={styles.menuToggle} />
-
             <div className={styles.modal}>
                 <nav className={styles.nav}>
-                <Link href="/sobre" passHref><span className={styles.link}>Sobre</span></Link>
-                <Link href="/produtos" passHref><span className={styles.link}>Produtos</span></Link>
-                <Link href="/contato" passHref><span className={styles.link}>Contato</span></Link>
-                </nav>
-                <Link href="/comprar" passHref>
-                    <span className={styles.cta}>Compre Aqui</span>
-                </Link>
-            </div>
+                    <div className={styles.logoContainer}>
+                        <img src="/img/logo.png" alt="Logo" className={styles.logo} />
+                    </div>
+                    <div className={styles.navmobile}>
 
+
+                        <Link href="/Inicio" passHref><span className={styles.link}>Inicio</span></Link>
+                        <Link href="/Sobre" passHref><span className={styles.link}>Sobre</span></Link>
+                        <Link href="/contato" passHref><span className={styles.link}>Contato</span></Link>
+                    </div>
+                    <Link href="/comprar" passHref><span className={styles.ctamobile}>Peça Já</span></Link>
+                </nav>
+            </div>
             <label htmlFor="menu-toggle" className={styles.menuIcon}>
                 <span></span>
                 <span></span>
